7f508675763fd76ca7a4b7e860c63c9208efdb84
k-aoshima <zakuzaku083@gmail.com>
2024-03-10 22:40:13 +0900

    修正と調整

--- src/MyPage.js
+++ src/MyPage.js
@@ -2,8 +2,11 @@ import React, {useState, useEffect} from 'react'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from './firebase'
 import { useNavigate, Navigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
 import TaskManagement from './TaskManagement';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box } from '@mui/system';
+import { Image } from '@mui/icons-material';
 
 function MyPage() {
   const [user, setUser] = useState("");
@@ -24,22 +27,45 @@ function MyPage() {
   }
 
   return (
-    <>
-    {!loading && (      
-      <>
-        {!user ? (
-          <Navigate to={`/login/`} />
-        ) : (
-          <>
-            <h1>マイページ</h1>
-            <p>{user?.email}</p>
-            <Button  type="submit" variant="contained" onClick={logout}>ログアウト</Button>
-            <TaskManagement />
-          </>
-        )}
-      </>
-    )}
-    </>
+    <Box>
+      <AppBar 
+        position="fixed"
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            マイページ
+          </Typography>
+          <Box sx ={{ mr: 3 }}>
+            {!user.photoURL ? (
+              <Box>{user?.email}</Box>
+            ) : (
+              <Image src={user?.photoURL} alt="Icon Image"/>
+            )}
+          </Box>
+          <Button color="inherit" onClick={logout}>ログアウト</Button>
+        </Toolbar>
+      </AppBar>
+      {!loading && (      
+        <Box sx={{ mt: 10 }}>
+          {!user ? (
+            <Navigate to={`/login/`} />
+          ) : (
+            <Box>
+              <TaskManagement />
+            </Box>
+          )}
+        </Box>
+      )}
+    </Box>
   )
 }
 
